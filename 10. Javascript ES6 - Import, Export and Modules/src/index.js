466d1b3ba4ace37684fb39c663d304ef8376cd85
Lorenzo Cattapan <47848907+LorenzoCattapan@users.noreply.github.com>
2024-10-27 09:49:36 +0000

    Javascript ES6 - Import, Export and Modules

---
+++ 10. Javascript ES6 - Import, Export and Modules/src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import * as pi from "./math.js";
+//import pi, { doublePi, triplePi } from "./math.js";
+
+console.log(pi);
+
+ReactDOM.render(
+  <ul>
+    <li>{pi.default}</li>
+    <li>{pi.doublePi()}</li>
+    <li>{pi.triplePi()}</li>
+  </ul>,
+  document.getElementById("root")
+);
