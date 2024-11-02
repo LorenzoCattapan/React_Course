94f816b7707ffd62f4177f94abeeec65fbce794e
Lorenzo Cattapan <47848907+LorenzoCattapan@users.noreply.github.com>
2024-11-02 14:51:22 +0100

    Javascript ES6 Spread Operator

---
+++ 29. Javascript ES6 Spread Operator/src/components/App.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+
+function App() {
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: ""
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setContact(prevValue => {
+        //RETURNS THE SAME CONTACT OBJ AND CHANGES ONLY THE ATTRIBUTE INDICATED BY NAME
+        return {
+            ...prevValue,
+            [name]: value,
+        }
+    });
+  }
+
+  return (
+    <div className="container">
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
+      <form>
+        <input
+          onChange={handleChange}
+          name="fName"
+          value={contact.fName}
+          placeholder="First Name"
+        />
+        <input
+          onChange={handleChange}
+          name="lName"
+          value={contact.lName}
+          placeholder="Last Name"
+        />
+        <input
+          onChange={handleChange}
+          name="email"
+          value={contact.email}
+          placeholder="Email"
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
